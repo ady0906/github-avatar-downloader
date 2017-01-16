ca38f0dcf1f8f622b12095a2f75d690c8e37c1d4
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-16 21:28:02 +0000

    Got the program to create the requestURL which leads to the API with the users' info

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
 
+var GITHUB_USER = "ady0906";
+var GITHUB_TOKEN = "5cd13c875f27ee5763bb017f7526c84a5eef9f6f";
+
+
 console.log('Welcome to the Github Avatar Downloader');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors", err);
+  console.log("Result", err);
+});
