1c5686ed3d1ac879d4f5c6489eec1b4d12f6f7ad
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-16 22:28:19 +0000

    Fetched the API body and logged it to the console

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,41 @@
 var request = require('request');
 
 var GITHUB_USER = "ady0906";
-var GITHUB_TOKEN = "5cd13c875f27ee5763bb017f7526c84a5eef9f6f";
+var GITHUB_TOKEN = "082a33fc158715ca3f872c3a2a33ff0c84479701";
 
 
 console.log('Welcome to the Github Avatar Downloader');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-}
+  // console.log(requestURL);
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'github-avatar-downloader'
+    }
+  };
+
+  // console.log(requestURL);
+
+  function parsingJSON(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      console.log(info);
+    }
+  }
+
+  request.get(options, parsingJSON)
+    .on('error', function(err) {
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+
+      //console.log('response', response);
+    });
+  }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
